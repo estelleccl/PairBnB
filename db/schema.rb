ee170dd65777288a93617bbde4cc6eb45add439c
Estelle <estelleccl@gmail.com>
2016-01-12 18:27:53 +0800

    added encrypted_password column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160112070310) do
+ActiveRecord::Schema.define(version: 20160112102552) do
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "encrypted_password"
   end
 
 end
