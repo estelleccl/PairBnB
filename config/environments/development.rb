db94f4fe73b4655928c6b5738dd7602f9dd424f8
Estelle <estelleccl@gmail.com>
2016-01-12 19:49:55 +0800

    forget password up and running

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,4 +39,17 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
+
+  ActionMailer::Base.delivery_method = :smtp
+
+  ActionMailer::Base.smtp_settings = {
+   :address => "smtp.gmail.com",
+   :port => 587,
+   :domain => "gmail.com",
+   :user_name => ENV["email_address"],
+   :password => ENV["email_password"],
+   :authentication => :plain,
+   :enable_starttls_auto => true
+}
+
 end
